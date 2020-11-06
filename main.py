b989ca7e24c5a351212a26ecb3a89ff32f749ccf
Thai Duong <me@thaiduong.org>
2020-11-06 11:04:58 +0700

    Day la file main

---
+++ main.py
@@ -0,0 +1,17 @@
+from requests_html import HTMLSession
+
+# Check Hotmail
+HOTMAIL = "thaiduong345456@hotmail.com"
+post_data = {
+    "email": HOTMAIL
+}
+session = HTMLSession()
+response = session.post("https://tools-trickfb.herokuapp.com/api/v1/check-mail", data=post_data)
+print(response.text)
+
+# Check yahoo
+YAHOO = "thaiduong34545@yahoo.com"
+session = HTMLSession()
+response = session.get(f"https://mmo69.com/_check_live_email/api.php?email={YAHOO}")
+print(response.text)
+
