2c532ac93c4b2e55b100aaed0731816cf3cc2d09
Minh Hùng <73692771+zallero1@users.noreply.github.com>
2020-11-12 15:03:14 +0700

    update contest.java

--- ContestManamentSystem/src/Contest.java
+++ ContestManamentSystem/src/Contest.java
@@ -1,5 +1,8 @@
+
 import java.util.*;
+
 public class Contest {
+
     public String ID;
 
     public Contest() {
@@ -16,10 +19,46 @@ public class Contest {
     public void setID(String ID) {
         this.ID = ID;
     }
-    QuestionBank qtBank = new QuestionBank();
-    ArrayList<Problem> rplst = qtBank.getLstProblems();
-   
-    
-    
-    
+
+    void generateNewContest() {
+        QuestionBank qtBank = new QuestionBank();
+        ArrayList<Problem> rplst = new ArrayList<>();
+        rplst = qtBank.getLstProblems();
+        ArrayList<Problem> contestTemp = new ArrayList<>();
+        contestTemp.add
+
+        }
+
+    }
+    public Problem getRandProb  (String category,int numb)
+    {   int temp=0;
+        Problem tempProb=new Problem();
+        Random rand = new Random();
+        QuestionBank qtBank = new QuestionBank();
+        ArrayList<Problem> rplst = qtBank.getLstProblems();
+        switch(category)
+        {
+            case"MAE":
+                numb=0;
+                for(Problem p : rplst)
+                {
+                    if (p.getCategory()=="MAE"&& rand.nextInt(2)==1)
+                    {
+                        tempProb=p;
+                        temp++;
+                        rplst.remove(p);
+                        if(temp>=numb)
+                        {
+                            break;
+                        }
+                    }
+                        
+                }break;
+            case"SQL":break;
+            case"PRF":
+            case"CEA":
+            case"CSI":
+        }
+    }
+
 }
