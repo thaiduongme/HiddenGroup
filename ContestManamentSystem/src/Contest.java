c5935ee29bb560fd094535523b17fb7d53a087f7
Minh Hùng <73692771+zallero1@users.noreply.github.com>
2020-11-12 18:17:17 +0700

    contest class

--- ContestManamentSystem/src/Contest.java
+++ ContestManamentSystem/src/Contest.java
@@ -1,8 +1,16 @@
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Contest {
 
+    ArrayList<Problem> rplst = new ArrayList<>();
+    ArrayList<Problem> contestTemp = new ArrayList<>();
     public String ID;
 
     public Contest() {
@@ -20,45 +28,125 @@ public class Contest {
         this.ID = ID;
     }
 
-    void generateNewContest() {
+    protected void generateNewContest() {
+
+        Random rd = new Random();
+        ID = String.format("%06d", rd.nextInt(10000));
         QuestionBank qtBank = new QuestionBank();
-        ArrayList<Problem> rplst = new ArrayList<>();
+
         rplst = qtBank.getLstProblems();
-        ArrayList<Problem> contestTemp = new ArrayList<>();
-        contestTemp.add
 
-        }
-
+        getRandProb("MAE");
+        getRandProb("PRF");
+        getRandProb("SQL");
+        getRandProb("CSI");
+        getRandProb("CEA");
     }
-    public Problem getRandProb  (String category,int numb)
-    {   int temp=0;
-        Problem tempProb=new Problem();
+
+    public void getRandProb(String category) {
+        
         Random rand = new Random();
-        QuestionBank qtBank = new QuestionBank();
-        ArrayList<Problem> rplst = qtBank.getLstProblems();
-        switch(category)
-        {
-            case"MAE":
-                numb=0;
-                for(Problem p : rplst)
-                {
-                    if (p.getCategory()=="MAE"&& rand.nextInt(2)==1)
-                    {
-                        tempProb=p;
-                        temp++;
-                        rplst.remove(p);
-                        if(temp>=numb)
-                        {
-                            break;
+        boolean check = false;
+        switch (category) {
+            case "MAE":
+                while (!check) {
+                    for (Problem p : rplst) {
+                        if (p.getCategory() == "MAE" && rand.nextInt(6) == 1) {
+                            contestTemp.add(p);
+                            rplst.remove(p);
+                            check = true;
                         }
                     }
-                        
-                }break;
-            case"SQL":break;
-            case"PRF":
-            case"CEA":
-            case"CSI":
-        }
+                }
+                break;
+            case "SQL":
+                while (!check) {
+                    for (Problem p : rplst) {
+                        if (p.getCategory() == "SQL" && rand.nextInt(6) == 1) {                           
+                            contestTemp.add(p);
+                            rplst.remove(p);
+                            check = true;
+                        }
+
+                    }
+                }
+                break;
+            case "PRF":
+                while (!check) {
+                    for (Problem p : rplst) {
+                        if (p.getCategory() == "PRF" && rand.nextInt(6) == 1) {                          
+                            contestTemp.add(p);
+                            rplst.remove(p);
+                            check = true;
+                        }
+                    }
+                }
+                break;
+            case "CEA":
+                while (!check) {
+                    for (Problem p : rplst) {
+                        if (p.getCategory() == "CEA" && rand.nextInt(6) == 1) {                          
+                            contestTemp.add(p);
+                            rplst.remove(p);
+                            check = true;
+                        }
+                    }
+                }
+                break;
+            case "CSI":
+                while (!check) {
+                    for (Problem p : rplst) {
+                        if (p.getCategory() == "CSI" && rand.nextInt(6) == 1) {                           
+                            contestTemp.add(p);
+                            rplst.remove(p);
+                            check = true;
+                        }
+                    }
+                }
+                break;
+        }      
     }
 
+    protected void printContest() {
+        System.out.println(rplst);
+    }
+
+    public void save() {
+        try {
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(ID + ".txt"));
+            String output = "";
+            for (Problem p : rplst) {
+                output += p.getCategory() + "|" + p.getID() + "|" + p.getProbName() + "|" + p.getShortDesc() + "|" + p.getFullDescLink()
+                        + "|" + p.getWeight() + "|" + p.getAuthor() + "|" + p.getCreatedDate() + "\n";
+            }
+            writer.write(output.trim());
+            writer.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        loadContest(ID+".txt");
+    }
+    protected void loadContest(String path)
+    {
+        contestTemp.clear();
+        try {
+            File fileDir = new File(path);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(fileDir), "UTF8"));
+
+            // Đọc từng dòng một trong file QuestionBank & add vào lstProblems
+            // Format từng dòng: category|ID|probName|shortDesc|fullDescLink|weight|author|createdDate
+            String str;
+            while ((str = in.readLine()) != null) {
+                contestTemp.add(new Problem(str.split("\\|")[0], str.split("\\|")[1], str.split("\\|")[2], str.split("\\|")[3],
+                        str.split("\\|")[4], Double.parseDouble(str.split("\\|")[5]), str.split("\\|")[6], str.split("\\|")[7]));
+            }
+
+            in.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
